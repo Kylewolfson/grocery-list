1a4d5810fdc591053b9363a6a003823ee3131a1b
Kyle Wolfson and Sam Durfey <sam@>
2016-03-28 10:53:33 -0700

    alphabetization works now, and there are no blank list items.

--- js/script.js
+++ js/script.js
@@ -1,14 +1,17 @@
 $(document).ready(function(){
+  var organizedList = [];
   $("form").submit(function(event) {
     event.preventDefault();
     var groceryList = ["item1", "item2", "item3", "item4", "item5", "item6"];
     groceryList.forEach(function (item) {
-      var organizedList = [];
-      organizedList.push("<li>" + ($("#" + item).val()).toUpperCase() + "</li>");
-      organizedList.sort();
-      console.log(organizedList);
-      $("#grocery-list").append(organizedList);
+      // The if statement is a test for truthiness. it will not add empty form items to the list.
+      if ($("#" + item).val()) {
+        organizedList.push("<li>" + ($("#" + item).val()).toUpperCase() + "</li>");
+        organizedList.sort();
+        console.log(organizedList);
+      }
     });
+    $("#grocery-list").append(organizedList);
     // make grocery list etc.
 
 
